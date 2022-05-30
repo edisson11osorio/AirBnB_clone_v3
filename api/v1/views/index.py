f9d4ca3652fc18c4e421960d6acff2dd8b82f106
joacasallas <joacasallas@gmail.com>
2022-05-29 20:43:52 -0500

    task 5. Some stats? => files = [api/v1/views/index.py] [Create an endpoint that retrieves the number of each objects by type]

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,21 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
-def status_json():
+def status():
     """status_json_response"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """retrieves the number of each objects by type"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
