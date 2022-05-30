1d7485e7b6dfb36be98d4896206219d6c823844c
edisson11osorio <osorioedisson08@gmail.com>
2022-05-30 15:59:32 -0500

    update✅

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" start API"""
+""" 
+start API
+"""
 
-
-from flask import Flask, jsonify, make_response
+from flask import Flask
 from models import storage
+
 from api.v1.views import app_views
 from os import getenv
 
-
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
 
 
@@ -21,7 +23,7 @@ def teardown_storage(self):
 @app.errorhandler(404)
 def page_not_found(error):
     """returns a JSON-formatted 404"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+    return {"error": "Not found"}, 404
 
 
 if __name__ == '__main__':
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,23 +2,25 @@
 """create a api"""
 
 
-from flask import jsonify
 from api.v1.views import app_views
+
+from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
-def status():
+@app_views.route('/status')
+def status_route():
     """status_json_response"""
     return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stats():
+@app_views.route('/stats')
+def stats_route():
     """retrieves the number of each objects by type"""
-    return jsonify({"amenities": storage.count("Amenity"),
+    return (jsonify({"amenities": storage.count("Amenity"),
                     "cities": storage.count("City"),
                     "places": storage.count("Place"),
                     "reviews": storage.count("Review"),
                     "states": storage.count("State"),
-                    "users": storage.count("User")})
+                    "users": storage.count("User")
+                    }))
