cba78e572bdb13fe44b4b1675814b846d3f9ca06
joacasallas <joacasallas@gmail.com>
2022-05-30 19:29:58 -0500

    fix pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,10 +17,11 @@ def status_route():
 @app_views.route('/stats')
 def stats_route():
     """retrieves the number of each objects by type"""
-    return (jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")
-                    }))
+    return (jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }))
